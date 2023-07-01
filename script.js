23f92db073d137f5d6ba968e37b92fdc649ddc55
Huseyn <117933054+husseinabdin@users.noreply.github.com>
2023-07-01 23:10:31 +0400

    MovieFIlmSite
    For my Portfolio

---
+++ script.js
@@ -0,0 +1,48 @@
+var swiper = new Swiper(".popular-content", {
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        280: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        510: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        758: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        900: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+    }
+});
+
+let playButton = document.querySelector(".play-movie");
+let video = document.querySelector(".video-container");
+let myvideo = document.querySelector("#myvideo");
+let closebtn = document.querySelector(".close-video");
+
+playButton.onclick = () => {
+    video.classList.add("show-video");
+    myvideo.play();
+};
+
+closebtn.onclick = () => {
+    video.classList.remove("show-video");
+    myvideo.pause();
+};
